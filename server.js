21bc3d2db7dc9ed2da4ca51d1e0da538093da014
Rohan89283 <rohanafk@black.com>
2026-03-16 02:53:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
+
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port " + PORT);
 });
